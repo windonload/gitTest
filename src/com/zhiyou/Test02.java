a221ee2f0dcff98978f125dbba8db2af76f5cff3
Administrator <Administrator@5CFBNWZ9GPSPXVV>
2018-01-05 16:48:24 +0800

    新增文件Test02

---
+++ src/com/zhiyou/Test02.java
@@ -0,0 +1,14 @@
+package com.zhiyou;
+
+/**
+ * @ClassName: Test02
+ * @Description: TODO
+ * @author: Administrator
+ * @date 2018年1月5日 下午4:46:56
+ * 
+ */
+public class Test02 {
+	public static void main(String[] args) {
+		System.out.println("新增文件");
+	}
+}
