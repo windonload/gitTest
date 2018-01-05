c707fd23b972b1f6c82c7b7de74ca686e15d79ca
Administrator <Administrator@5CFBNWZ9GPSPXVV>
2018-01-05 16:46:01 +0800

    新建了项目

---
+++ src/com/zhiyou/Test.java
@@ -0,0 +1,14 @@
+package com.zhiyou;
+
+/**
+ * @ClassName: Test
+ * @Description: TODO
+ * @author: Administrator
+ * @date 2018年1月5日 下午4:41:36
+ * 
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("测试项目");
+	}
+}
